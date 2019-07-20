b6ecb9613c0827d64ef1a48f51b4fe45c8663663
alauhy <751961605@qq.com>
2019-07-20 23:27:04 +0800

    [LXY]:implement header

---
+++ Vue-Todo-List/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+
+  height: auto;
+  width: 800px;
+  border:1px solid black;
+  border-radius: 15px;
+  margin-left: 30%;
+
+}
+</style>z
---
+++ Vue-Todo-List/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="header">
+        <p id="title">Jquery To Do List</p>
+        <i id="description">Simple Todo List with adding and filter by diff status</i>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+    #title{
+        margin-left: 10px;
+        font-size: 25px;
+        font-weight: bold;
+        font-family: "Times New Roman";
+    }
+    #description{
+        margin-left: 10px;
+        font-size: 20px;
+        font-family: "Times New Roman";
+    }
+
+
+
+
+</style>
