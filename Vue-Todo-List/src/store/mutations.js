79d2d5cfb121befad349ed46c4e0d69f101f07cb
alauhy <751961605@qq.com>
2019-07-24 11:16:18 +0800

    [LXY]:refactor mutations

--- Vue-Todo-List/src/store/mutations.js
+++ Vue-Todo-List/src/store/mutations.js
@@ -1,37 +1,46 @@
+const toSetList = (state, list) => {
+    state.todolist = list
+}
+const addToList = (state, event) => {
+    state.todolist.push(event)
+}
+const setStatusFalse = (state, index) => {
+    state.todolist[index].status = false;
+}
+const setEditableFalse = (state, index) => {
+    state.todolist[index].editable = false;
+}
+const setEditableTrue = (state, index) => {
+    state.todolist[index].editable = true;
+}
+const setStatusTrue = (state, index) => {
+    state.todolist[index].status = true;
+}
+const atAll = (state) => {
+    state.todolist.map(e => e.isShow = true);
+}
+const atActive=(state)=> {
+    state.todolist.map(e => {
+        e.status ? e.isShow = false : e.isShow = true
+    });
 
+}
+const atComplete=(state)=> {
+    state.todolist.map(e => {
+        e.status ? e.isShow = true : e.isShow = false
+    });
+
+}
 export default {
-    toSetList(state,list){
-        state.todolist = list
-    },
-    addToList(state,event) {
-        state.todolist.push(event)
-    },
-    setStatusFalse(state,index) {
-        state.todolist[index].status = false;
-    },
-    setEditableFalse(state,index) {
-        state.todolist[index].editable = false;
-    },
-    setEditableTrue(state,index) {
-        state.todolist[index].editable = true;
-    },
+    toSetList,
+    addToList,
+    setStatusFalse,
+    setEditableFalse,
+    setEditableTrue,
+    setStatusTrue,
+    atAll,
+    atActive,
+    atComplete
 
-    setStatusTrue(state,index) {
-        state.todolist[index].status = true;
-    },
-    atAll (state){
-        state.todolist.map(e => e.isShow = true);
-    },
-    atActive(state) {
-        state.todolist.map(e => {
-            e.status ? e.isShow = false : e.isShow = true
-        });
 
-    },
-    atComplete(state) {
-        state.todolist.map(e => {
-            e.status ? e.isShow = true : e.isShow = false
-        });
-
-    }
 }
